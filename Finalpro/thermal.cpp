38847b2314eb14b60cf1c600fc4499aeda6b3ada
Hyun <gusdn9706@gmail.com>
2024-06-17 15:07:50 +0900

    온도범위 초과알림코드 추가

--- Finalpro/thermal.cpp
+++ Finalpro/thermal.cpp
@@ -4,6 +4,7 @@ Thermal::Thermal() : oneWire(D2), dallasTemperature(&oneWire)
 {
     this->temperature = 0;
     this->pin = D2;
+    this->hot_temp = 35.0, this->cold_temp = 15.0;
 }
 
 void Thermal::begin()
@@ -15,5 +16,27 @@ void Thermal::begin()
 float Thermal::getTemperature()
 {
     dallasTemperature.requestTemperatures();
-    return dallasTemperature.getTempCByIndex(0);
+    this->temperature = dallasTemperature.getTempCByIndex(0);
+    return this->temperature;
+}
+
+void Thermal::set_alarm_temp(float hot_temp, float cold_temp)
+{
+    this->hot_temp = hot_temp;
+    this->cold_temp = cold_temp;
+}
+
+uint8_t Thermal::check_alarm()
+{
+    if(this->temperature > this->hot_temp)
+    {
+        return 1;
+    }
+    else if(this->temperature < this->cold_temp)
+    {
+        return 2;
+    }
+    else
+        return 0;
+    
 }
--- Finalpro/thermal.h
+++ Finalpro/thermal.h
@@ -4,17 +4,17 @@
 #ifndef THERMAL_H
 #define THERMAL_H
 
-
-
 class Thermal{
     public:
         Thermal();
         void begin();
         float getTemperature();
-        
+        void set_alarm_temp(float hot_temp,float cold_temp);
+        uint8_t check_alarm();
     private:
         float temperature;
         int pin;
+        float hot_temp, cold_temp;
         OneWire oneWire;
         DallasTemperature dallasTemperature;
 };
